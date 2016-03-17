4e21ac3af62706e88efdbcfcdc334f53060c5ec1
Michael Voetter <mike@famv.net>
2016-03-17 19:55:19 +0100

    add nb make file parts

---
+++ mcc/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=mcc
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/mcc
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=mcc.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/mcc.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=mcc
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/mcc
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=mcc.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/mcc.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
