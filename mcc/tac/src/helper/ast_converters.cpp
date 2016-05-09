b2533e6e7614359eb6a238b81b0d84d0609fd8b1
Michael Voetter <Michael.Voetter@student.uibk.ac.at>
2016-05-09 14:59:52 +0200

    rename convert functions to match corresponding ast node name

--- mcc/tac/src/helper/ast_converters.cpp
+++ mcc/tac/src/helper/ast_converters.cpp
@@ -55,10 +55,10 @@ Operand::ptr_t convertNode(Tac *t, AstNode n) {
   if (isType<ast::decl_stmt>(n)) return convertDeclStmt(t, n);
   if (isType<ast::if_stmt>(n)) return convertIfStmt(t, n);
   if (isType<ast::while_stmt>(n)) return convertWhileStmt(t, n);
+  if (isType<ast::functionList>(n)) return convertFunctionList(t, n);
+  if (isType<ast::function_def>(n)) return convertFunctionDef(t, n);
+  if (isType<ast::functionCall_expr>(n)) return convertfunctionCallExpr(t, n);
   if (isType<ast::return_stmt>(n)) return convertReturnStmt(t, n);
-  if (isType<ast::functionList>(n)) return convertFunctionDefList(t, n);
-  if (isType<ast::function_def>(n)) return convertFunctionDefinition(t, n);
-  if (isType<ast::functionCall_expr>(n)) return convertFunctionCall(t, n);
 
   // Debugging output; this is only printed if something goes terribly
   // wrong
@@ -251,25 +251,7 @@ Operand::ptr_t convertWhileStmt(Tac *t, AstNode n) {
   return nullptr;
 }
 
-Operand::ptr_t convertReturnStmt(Tac *t, AstNode n) {
-  auto ret = std::static_pointer_cast<ast::return_stmt>(n);
-  if (ret->returnValue != nullptr) {
-    auto returnValue = convertNode(t, ret->returnValue);
-    auto op = Operator(OperatorName::RET);
-    auto retTriple = std::make_shared<Triple>(op, returnValue);
-    t->addLine(retTriple);
-    return retTriple;
-
-  } else {
-    auto op = Operator(OperatorName::RET);
-    auto retTriple = std::make_shared<Triple>(op, nullptr);
-    retTriple->setType(Type::NONE);
-    t->addLine(retTriple);
-    return retTriple;
-  }
-}
-
-Operand::ptr_t convertFunctionDefList(Tac *t, AstNode n) {
+Operand::ptr_t convertFunctionList(Tac *t, AstNode n) {
   auto v = std::static_pointer_cast<ast::functionList>(n);
 
   for (auto fun : v->functions) {
@@ -279,7 +261,7 @@ Operand::ptr_t convertFunctionDefList(Tac *t, AstNode n) {
   return nullptr;
 }
 
-Operand::ptr_t convertFunctionDefinition(Tac *t, AstNode n) {
+Operand::ptr_t convertFunctionDef(Tac *t, AstNode n) {
   auto function = std::static_pointer_cast<ast::function_def>(n);
   auto codelines = t->codeLines.size();
 
@@ -340,7 +322,7 @@ Operand::ptr_t convertFunctionDefinition(Tac *t, AstNode n) {
   return nullptr;
 }
 
-Operand::ptr_t convertFunctionCall(Tac *t, AstNode n) {
+Operand::ptr_t convertfunctionCallExpr(Tac *t, AstNode n) {
   auto functionCall = std::static_pointer_cast<ast::functionCall_expr>(n);
 
   if (functionCall->arguments.size() !=
@@ -388,6 +370,24 @@ Operand::ptr_t convertFunctionCall(Tac *t, AstNode n) {
   t->nextBasicBlock();
 
   return callTriple;
+}
+
+Operand::ptr_t convertReturnStmt(Tac *t, AstNode n) {
+  auto ret = std::static_pointer_cast<ast::return_stmt>(n);
+  if (ret->returnValue != nullptr) {
+    auto returnValue = convertNode(t, ret->returnValue);
+    auto op = Operator(OperatorName::RET);
+    auto retTriple = std::make_shared<Triple>(op, returnValue);
+    t->addLine(retTriple);
+    return retTriple;
+
+  } else {
+    auto op = Operator(OperatorName::RET);
+    auto retTriple = std::make_shared<Triple>(op, nullptr);
+    retTriple->setType(Type::NONE);
+    t->addLine(retTriple);
+    return retTriple;
+  }
 }
 }
 }
