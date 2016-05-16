e2788d093669719dc233e7fa3d4bce0003865524
Philipp Stiegernigg <philipp.stiegernigg@gmail.com>
2016-05-16 17:50:59 +0200

    Added assembly conversion of return (without float literal support)

--- mcc/gas/src/gas.cpp
+++ mcc/gas/src/gas.cpp
@@ -7,8 +7,13 @@
 #include <typeinfo>
 
 #include "mcc/gas/x86_instruction_set.h"
+#include "mcc/tac/float_literal.h"
+#include "mcc/tac/float_literal.h"
+#include "mcc/tac/helper/ast_converters.h"
+#include "mcc/tac/int_literal.h"
 #include "mcc/tac/operator.h"
 #include "mcc/tac/triple.h"
+#include "mcc/tac/variable.h"
 
 namespace mcc {
 
@@ -102,8 +107,30 @@ Gas::Gas(Tac tac) : functionMap(tac.getFunctionMap()) {
           }
         }
 
+        unsigned Gas::lookupFunctionStackSize(std::string functionName) {
+          auto found = functionStackSpaceMap->find(functionName);
+
+          if (found != functionStackSpaceMap->end()) {
+            return found->second;
+          } else {
+            return 0;
+          }
+        }
+
+        unsigned Gas::lookupVariableStackOffset(Variable::ptr_t var,
+                                                std::string functionName) {
+          auto found = variableStackOffsetMap->find(var);
+
+          if (found != variableStackOffsetMap->end()) {
+            return found->second + lookupFunctionArgSize(functionName) + 4;
+          } else {
+            return 0;
+          }
+        }
+
         void Gas::convertTac(Tac &tac) {
           this->analyzeTac(tac);
+          Label::ptr_t currentFunction = std::make_shared<Label>();
 
           for (auto triple : tac.codeLines) {
             auto op = triple->getOperator();
@@ -129,7 +156,7 @@ Gas::Gas(Tac tac) : functionMap(tac.getFunctionMap()) {
                 break;
 
               case OperatorName::LABEL:
-                convertLabel(triple);
+                convertLabel(triple, currentFunction);
                 break;
 
               case OperatorName::JUMP:
@@ -163,19 +190,23 @@ Gas::Gas(Tac tac) : functionMap(tac.getFunctionMap()) {
                 break;
 
               case OperatorName::RET:
-                convertReturn(triple);
+                convertReturn(triple, currentFunction);
                 break;
             }
             }
         }
 
-        void Gas::convertLabel(Triple::ptr_t triple) {
+        void Gas::convertLabel(Triple::ptr_t triple,
+                               Label::ptr_t currentFunction) {
           auto labelTriple = std::static_pointer_cast<Label>(triple);
 
           auto label = std::make_shared<Mnemonic>(labelTriple->getName());
           asmInstructions.push_back(label);
 
+          // Add function entry
           if (labelTriple->isFunctionEntry()) {
+            *currentFunction.get() = *labelTriple.get();
+
             auto ebp = std::make_shared<Operand>(Register::EBP);
             auto esp = std::make_shared<Operand>(Register::ESP);
 
@@ -183,6 +214,16 @@ Gas::Gas(Tac tac) : functionMap(tac.getFunctionMap()) {
                 std::make_shared<Mnemonic>(Instruction::PUSH, ebp));
             asmInstructions.push_back(
                 std::make_shared<Mnemonic>(Instruction::MOV, ebp, esp));
+
+            // Make space on stack for variables
+            unsigned stackSize =
+                lookupFunctionStackSize(labelTriple->getName());
+
+            if (stackSize > 0) {
+              auto stackspaceOp = std::make_shared<Operand>(stackSize);
+              asmInstructions.push_back(std::make_shared<Mnemonic>(
+                  Instruction::SUB, esp, stackspaceOp));
+            }
           }
         }
 
@@ -210,7 +251,52 @@ Gas::Gas(Tac tac) : functionMap(tac.getFunctionMap()) {
           }
         }
 
-        void Gas::convertReturn(Triple::ptr_t triple) {}
+        void Gas::convertReturn(Triple::ptr_t triple,
+                                Label::ptr_t currentFunction) {
+          auto eax = std::make_shared<Operand>(Register::EAX);
+          auto ebp = std::make_shared<Operand>(Register::EBP);
+          auto esp = std::make_shared<Operand>(Register::ESP);
+
+          if (triple->containsArg1()) {
+            auto op = triple->getArg1();
+
+            if (helper::isType<IntLiteral>(op)) {
+              auto intOp = std::static_pointer_cast<IntLiteral>(op);
+              auto asmInt = std::make_shared<Operand>(intOp->value);
+
+              asmInstructions.push_back(
+                  std::make_shared<Mnemonic>(Instruction::MOV, eax, asmInt));
+
+            } else if (helper::isType<FloatLiteral>(op)) {
+              /*TODO*/
+
+            } else if (helper::isType<Variable>(op)) {
+              auto variableOp = std::static_pointer_cast<Variable>(op);
+              unsigned varOffset = lookupVariableStackOffset(
+                  variableOp, currentFunction->getName());
+              auto asmVar = std::make_shared<Operand>(Register::EBP, varOffset);
+
+              asmInstructions.push_back(
+                  std::make_shared<Mnemonic>(Instruction::MOV, eax, asmVar));
+            }
+          }
+
+          unsigned stackSize =
+              lookupFunctionStackSize(currentFunction->getName());
+
+          // Cleanup stack
+          if (stackSize > 0) {
+            auto stackspaceOp = std::make_shared<Operand>(stackSize);
+            asmInstructions.push_back(std::make_shared<Mnemonic>(
+                Instruction::ADD, esp, stackspaceOp));
+          }
+
+          asmInstructions.push_back(
+              std::make_shared<Mnemonic>(Instruction::POP, ebp));
+
+          asmInstructions.push_back(
+              std::make_shared<Mnemonic>(Instruction::RET));
+        }
 
         std::string Gas::toString() const {
           std::ostringstream stream;
