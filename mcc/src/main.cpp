aed4770061e9133ded815eacfa83e206f5505f9c
Michael Voetter <Michael.Voetter@student.uibk.ac.at>
2016-05-17 13:59:55 +0200

    add output to main again

--- mcc/src/main.cpp
+++ mcc/src/main.cpp
@@ -25,17 +25,58 @@ int main(int argc, char **argv) {
   auto source = buffer.str();
   auto tree = parser::parse(source);
 
+  if (!tree) {
+    std::cout << "File: " << argv[1] << " is not a valid mC file." << std::endl;
+    return -1;
+  }
+
+  formatted_ostream out(std::cout);
+  out << "Parsed:\n" << tree << "\n";
+
   Tac tac = Tac(tree);
-  // LVN::transform(tac);
+  std::cout << "Three-Adress Code:" << std::endl;
+  std::cout << tac.toString() << std::endl;
+
+  LVN::transform(tac);
+  std::cout << std::endl << "Three-Adress Code after LVN:" << std::endl;
+  std::cout << tac.toString() << std::endl;
 
   auto graph = std::make_shared<Cfg>(tac);
+  std::cout << std::endl << "BB:" << std::endl;
+
+  auto bbIndex = tac.getBasicBlockIndex();
+
+  graph->computeLiveInOut();
+  graph->computeWorkList();
+  for (auto const b : *bbIndex.get()) {
+    std::cout << "LiveIn: { ";
+    for (auto const in : graph->getLiveIn(b->getBlockId())) {
+      std::cout << in->getName() << ", ";
+    }
+    std::cout << "}" << std::endl;
+
+    std::cout << b->toString();
+
+    std::cout << "LiveOut: { ";
+    for (auto const out : graph->getLiveOut(b->getBlockId())) {
+      std::cout << out->getName() << ", ";
+    }
+    std::cout << "}" << std::endl << std::endl;
+  }
 
   // GNU Assembly
   std::cout << "GAS:" << std::endl;
   Gas gas = Gas(tac);
+
   auto map1 = gas.getFunctionStackSpaceMap();
   for (auto it = map1->begin(); it != map1->end(); ++it) {
     std::cout << it->first.get() << ": Stack Space = " << it->second
+              << std::endl;
+  }
+
+  auto map2 = gas.getVariableStackOffsetMap();
+  for (auto it = map2->begin(); it != map2->end(); ++it) {
+    std::cout << it->first->getValue() << ": Stack Offset = " << it->second
               << std::endl;
   }
 
