bb6a3530dd3b2f43b0beeab6d3b09a4eaae009a5
Philipp Stiegernigg <philipp.stiegernigg@gmail.com>
2016-06-05 12:57:53 +0200

    Fixed array tests for VLAS

--- mcc/test/tac/array_tests.cpp
+++ mcc/test/tac/array_tests.cpp
@@ -3,46 +3,49 @@
 #include <string>
 
 #include "mcc/tac/array.h"
+#include "mcc/tac/int_literal.h"
 
 namespace mcc {
 namespace tac {
-/*
+
 TEST(Array, Leaf) {
-  Array a = Array(Type::INT, "a", 5);
+  auto size = std::make_shared<IntLiteral>(5);
+  Array a = Array(Type::INT, "a", size);
   EXPECT_EQ(true, a.isLeaf());
 }
 
 TEST(Array, Type) {
-  Array a = Array(Type::INT, "a", 5);
+  auto size = std::make_shared<IntLiteral>(5);
+  Array a = Array(Type::INT, "a", size);
   EXPECT_EQ(Type::INT, a.getType());
 }
 
 TEST(Array, Value) {
   std::string name = "a";
-  std::size_t size = 5;
+ auto size = std::make_shared<IntLiteral>(5);
 
   Array a = Array(Type::INT, name, size);
 
   auto value = name;
   value.append("[");
-  value.append(std::to_string(size));
+  value.append(size->getValue());
   value.append("]");
 
   EXPECT_EQ(value, a.getValue());
 }
 
 TEST(Array, Name) {
-  Array a = Array(Type::INT, "a", 5);
+  auto size = std::make_shared<IntLiteral>(5);
+  Array a = Array(Type::INT, "a", size);
 
   EXPECT_EQ("a", a.getName());
 }
 
 TEST(Array, Length) {
-  std::size_t size = 5;
-
+  auto size = std::make_shared<IntLiteral>(5);
   Array a = Array(Type::INT, "a", size);
 
   EXPECT_EQ(size, a.length());
-}*/
+}
 }
 }
